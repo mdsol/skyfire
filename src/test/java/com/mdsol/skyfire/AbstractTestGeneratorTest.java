3a4f4feb18aa4f29ddf9b61af9faade58f2d6919
Nan Li <nli@mdsol.com>
2015-12-15 12:08:06 -0500

    Add constraints to the roc model

--- src/test/java/com/mdsol/skyfire/AbstractTestGeneratorTest.java
+++ src/test/java/com/mdsol/skyfire/AbstractTestGeneratorTest.java
@@ -5,7 +5,7 @@
  ******************************************************************************/
 package com.mdsol.skyfire;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 import java.util.Iterator;
@@ -25,7 +25,7 @@ import coverage.web.InvalidInputException;
 
 /**
  * A JUnit test case for class {@link AbstractTestGenerator}
- * 
+ *
  * @author Nan Li
  * @version 1.0 Nov 28, 2012
  *
@@ -39,9 +39,10 @@ public class AbstractTestGeneratorTest {
 
     /**
      * @throws java.lang.Exception
+     *             when exceptions happen
      */
     @Before
-    public void setUp() throws Exception {
+    public final void setUp() throws Exception {
         testResourceDir = System.getProperty("user.dir") + "/src/test/resources/testData/";
         vendingMachinePath = testResourceDir + "VendingMachine/model/VendingMachineFSM.uml";
         vendingMachineXmlPath = testResourceDir + "VendingMachine/xml/vendingMachineMappings.xml";
@@ -50,44 +51,55 @@ public class AbstractTestGeneratorTest {
 
     /**
      * @throws java.lang.Exception
+     *             when exceptions happen
      */
     @After
     public void tearDown() throws Exception {
     }
 
+    /**
+     *
+     * @throws IOException
+     *             when IO exceptions happen
+     * @throws InvalidInputException
+     *             when there are invalid inputs for a graph
+     * @throws InvalidGraphException
+     *             when the graph is invalid
+     */
     @Test
-    public void testGetTestPaths()
+    public final void testGetTestPaths()
             throws IOException, InvalidInputException, InvalidGraphException {
 
-        EObject object = StateMachineAccessor.getModelObject(vendingMachinePath);
-        List<StateMachine> statemachines = StateMachineAccessor.getStateMachines(object);
-        List<Region> regions = StateMachineAccessor.getRegions(statemachines.get(0));
-        StateMachineAccessor stateMachine = new StateMachineAccessor(regions.get(0));
+        final EObject object = StateMachineAccessor.getModelObject(vendingMachinePath);
+        final List<StateMachine> statemachines = StateMachineAccessor.getStateMachines(object);
+        final List<Region> regions = StateMachineAccessor.getRegions(statemachines.get(0));
+        final StateMachineAccessor stateMachine = new StateMachineAccessor(regions.get(0));
 
-        List<Path> paths = AbstractTestGenerator.getTestPaths(stateMachine.getEdges(),
+        final List<Path> paths = AbstractTestGenerator.getTestPaths(stateMachine.getEdges(),
                 stateMachine.getInitialStates(), stateMachine.getFinalStates(),
                 TestCoverageCriteria.PRIMEPATHCOVERAGE);
 
-        /*
-         * String[] edges = stateMachine.getEdges().split("\n"); for(String edge : edges){ String[]
-         * vertices = edge.split(" "); //System.out.println(vertices[0]+ " :: " + vertices[1]);
-         * System.out.println(stateMachine.getReversedStateMappings().get(vertices[0]).getName() +
-         * " :: " + stateMachine.getReversedStateMappings().get(vertices[1]).getName());
-         * //System.out.println(stateMachine.getReversedStateMappings().get(vertices[1]).getName());
-         * }
-         */
         assertNotNull(paths);
         System.out.println(paths);
     }
 
+    /**
+     *
+     * @throws IOException
+     *             when IO exceptions happen
+     * @throws InvalidInputException
+     *             when there are invalid inputs for a graph
+     * @throws InvalidGraphException
+     *             when the graph is invalid
+     */
     @Test
-    public void testGetTestPathsForRoc()
+    public final void testGetTestPathsForRoc()
             throws IOException, InvalidInputException, InvalidGraphException {
 
-        EObject object = StateMachineAccessor.getModelObject(rocPath);
-        List<StateMachine> statemachines = StateMachineAccessor.getStateMachines(object);
-        List<Region> regions = StateMachineAccessor.getRegions(statemachines.get(0));
-        StateMachineAccessor stateMachine = new StateMachineAccessor(regions.get(0));
+        final EObject object = StateMachineAccessor.getModelObject(rocPath);
+        final List<StateMachine> statemachines = StateMachineAccessor.getStateMachines(object);
+        final List<Region> regions = StateMachineAccessor.getRegions(statemachines.get(0));
+        final StateMachineAccessor stateMachine = new StateMachineAccessor(regions.get(0));
 
         System.out.println(statemachines.get(0));
         System.out.println(regions.get(0));
@@ -97,13 +109,12 @@ public class AbstractTestGeneratorTest {
 
         System.out.println(stateMachine.getStateMappings());
 
-        String[] edges = stateMachine.getEdges().split("\n");
+        final String[] edges = stateMachine.getEdges().split("\n");
         for (String edge : edges) {
-            String[] vertices = edge.split(" ");
+            final String[] vertices = edge.split(" ");
             // System.out.println(vertices[0]+ " :: " + vertices[1]);
             System.out.println(stateMachine.getReversedStateMappings().get(vertices[0]).getName()
                     + " :: " + stateMachine.getReversedStateMappings().get(vertices[1]).getName());
-            // System.out.println(stateMachine.getReversedStateMappings().get(vertices[1]).getName());
         }
 
         List<Path> paths = AbstractTestGenerator.getTestPaths(stateMachine.getEdges(),
@@ -113,9 +124,9 @@ public class AbstractTestGeneratorTest {
         assertNotNull(paths);
         System.out.println(paths);
         for (Path path : paths) {
-            Iterator<Node> nodes = path.getNodeIterator();
+            final Iterator<Node> nodes = path.getNodeIterator();
             while (nodes.hasNext()) {
-                String node = nodes.next().toString();
+                final String node = nodes.next().toString();
                 System.out.println(stateMachine.getReversedStateMappings().get(node).getName());
             }
             System.out.println("\n");
@@ -128,9 +139,9 @@ public class AbstractTestGeneratorTest {
         assertNotNull(paths);
         System.out.println(paths);
         for (Path path : paths) {
-            Iterator<Node> nodes = path.getNodeIterator();
+            final Iterator<Node> nodes = path.getNodeIterator();
             while (nodes.hasNext()) {
-                String node = nodes.next().toString();
+                final String node = nodes.next().toString();
                 System.out.println(stateMachine.getReversedStateMappings().get(node).getName());
             }
             System.out.println("\n");
