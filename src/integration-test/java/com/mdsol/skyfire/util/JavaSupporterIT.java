00a62cca54eab65ce7b075527f02366ec6cb8df1
Nan Li <nli@mdsol.com>
2016-03-28 17:50:24 -0400

    Fix critical issues reported by SonarQube in JavaSupporterIT

--- src/integration-test/java/com/mdsol/skyfire/util/JavaSupporterIT.java
+++ src/integration-test/java/com/mdsol/skyfire/util/JavaSupporterIT.java
@@ -7,19 +7,9 @@ package com.mdsol.skyfire.util;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class JavaSupporterIT {
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
 
     @Test
     public void testRemoveSemiColon() {
